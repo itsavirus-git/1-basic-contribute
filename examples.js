5c2e723a28c23ee359a4c91e3a3e65019a054f0a
ARSAN <arsan.personal@gmail.com>
2022-02-11 11:43:32 +0800

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,10 @@
+const json = {
+    "title": "Itsavirus Basic Contribute",
+    "users": [
+        "itsavirus-com",
+        "mtegarsantosa"
+        // <Add-Your-Username-Here>
+    ]
+}
+
+console.log(json);
